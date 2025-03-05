d6fc2499ff192528d4e118719e3ddec060b066b3
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-03-05 12:28:01 +0100

    refactor(keymap.c): Update chordal handedness definition

--- vq9an/keymap.c
+++ vq9an/keymap.c
@@ -93,18 +93,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Handedness for Chordal Hold.
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
-  LAYOUT_LR(
-  '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
-  'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-  'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-  'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-                                               '*'    , 'L'    ,
-
-                    '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
-                    'R'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    ,
-                    'R'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    ,
-                    'R'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    ,
-           '*'    , 'R'
+  LAYOUT(
+  '*'    , '*'    , '*'    , '*'    , '*'    , '*'    , '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
+  'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    ,
+  'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    ,
+  'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    , 'R'    ,
+                                      '*'    , 'L'    , '*'    , 'R'
 );
 // clang-format on
 
