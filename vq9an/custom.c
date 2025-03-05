6b44efa9e117c21a6f56ee957e2100447742df9c
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-03-05 15:26:32 +0100

    feat: Add key overrides

--- vq9an/custom.c
+++ vq9an/custom.c
@@ -7,3 +7,15 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
                    'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
                    '*', 'L', 'R', '*');
 // clang-format on
+
+// key overrides
+const key_override_t underscore_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_UNDS);
+
+const key_override_t bspc_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_BSPC);
+
+const key_override_t *key_overrides[] = {
+    &underscore_key_override, //
+    &bspc_override            //
+};
