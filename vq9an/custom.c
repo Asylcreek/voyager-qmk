f769851b74ceba9b988c707fcc66cfc643eaf0ef
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-03-07 22:21:01 +0100

    refactor(custom): Trigger hold on 2 thumb keys

--- vq9an/custom.c
+++ vq9an/custom.c
@@ -8,6 +8,22 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
                    '*', 'L', 'R', '*');
 // clang-format on
 
+bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record,
+                      uint16_t other_keycode, keyrecord_t *other_record) {
+
+  // Exceptionally allow some one-handed chords for hotkeys.
+  switch (tap_hold_keycode) {
+  case MT(MOD_LSFT, KC_BSPC):
+    return true;
+
+  case LT(1, KC_SPACE):
+    return true;
+  }
+
+  // Otherwise defer to the opposite hands rule.
+  return get_chordal_hold_default(tap_hold_record, other_record);
+}
+
 // key overrides
 const key_override_t shift_quote =
     ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_UNDS);
