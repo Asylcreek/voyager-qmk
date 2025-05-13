be5b6b9ee2c4867f299a993737ce6163e7341cd7
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-05-14 00:15:16 +0100

    refactor(keymap): Reduce tap flow term for special keys instead

--- KyQdl/keymap.c
+++ KyQdl/keymap.c
@@ -145,31 +145,22 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-bool is_tap_flow_key(uint16_t keycode) {
-  switch (keycode) {
-  case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-    keycode = QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
-    break;
-  case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-    keycode = QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
-    break;
+uint16_t get_tap_flow_term(uint16_t keycode, keyrecord_t *record,
+                           uint16_t prev_keycode) {
+  if (prev_keycode == KC_BSPC) {
+    return 0; // Disable filter when immediately following backspace.
   }
-  switch (keycode) {
-  case KC_SPC:
-  case KC_A ... KC_Z:
-  case KC_DOT:
-  case KC_COMM:
-  case KC_SCLN:
-  case KC_SLSH:
-    return true;
 
+  switch (keycode) {
   case HRM_S:
   case HRM_H:
   case HRM_SPC:
   case PRE_REPEAT:
-    return false;
+    return 0; // Disable filter for these keys.
+
+  default:
+    return g_tap_flow_term; // Longer timeout otherwise.
   }
-  return false;
 }
 
 bool caps_word_press_user(uint16_t keycode) {
