13594e2014dc7e38e233ade4f3970362a38b2d7e
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-05-20 20:00:10 +0100

    chore(7yGlw): Add custom.c

---
+++ 7yGlw/custom.c
@@ -0,0 +1,20 @@
+#include "features/custom_shift_keys.h"
+#include "features/select_word.h"
+
+// clang-format off
+// Handedness for Chordal Hold.
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT_voyager('*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
+                   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+                   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+                   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+                   '*', 'L', 'R', '*');
+// clang-format on
+
+// key overrides
+const custom_shift_key_t custom_shift_keys[] = {
+    {KC_QUOT, KC_UNDS}, // Shift ' is _
+};
+
+uint8_t NUM_CUSTOM_SHIFT_KEYS =
+    sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
