d6089743bd9ff3a3de215ea4339403619f1e90af
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-05-20 20:07:24 +0100

    chore(7yGlw): Change handedness

--- 7yGlw/custom.c
+++ 7yGlw/custom.c
@@ -4,11 +4,11 @@
 // clang-format off
 // Handedness for Chordal Hold.
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
-    LAYOUT_voyager('*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
+    LAYOUT_voyager('L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
                    'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
                    'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
-                   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
-                   '*', 'L', 'R', '*');
+                   'L', 'L', 'L', 'L', 'L', '*', '*', 'R', 'R', 'R', 'R', 'R',
+                   'L', 'L', 'R', 'R');
 // clang-format on
 
 // key overrides
