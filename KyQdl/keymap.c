06665ee8548f54942f3e75a589b1721ebfa6bcf9
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-07-07 20:10:25 +0100

    refactor: Update alt-repeat for dollar

--- KyQdl/keymap.c
+++ KyQdl/keymap.c
@@ -165,7 +165,6 @@ uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
   case KC_DOUBLE_QUOTE:
     return KC_PLUS;
 
-  case KC_DOLLAR:
   case S(KC_4):
     return M_ALT_DOLLAR;
 
