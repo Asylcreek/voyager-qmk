a047105433eb3c01ec8084e7100ebcfbe72452e0
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-07-10 20:28:15 +0100

    fix: Update process_record_user

--- KyQdl/keymap.c
+++ KyQdl/keymap.c
@@ -152,7 +152,7 @@ bool remember_last_key_user(uint16_t keycode, keyrecord_t *record,
   case PRE_REPEAT:
   case PRE_MAGIC:
   case KC_F23:
-  case MT(MOD_LSFT, KC_F23):
+    /* case MT(MOD_LSFT, KC_F23): */
     return false;
   };
 
