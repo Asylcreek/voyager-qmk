9c17856ff8e179bb8e70e76b747c5e2953738181
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-07-29 20:48:01 +0100

    refactor: Change LT key on layer 2 to layer 1

--- KyQdl/keymap.c
+++ KyQdl/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PIPE,        KC_ASTR,        KC_9,           KC_8,           KC_7,           KC_PLUS,                                        KC_UNDS,        KC_LBRC,        KC_LPRN,        KC_LCBR,        KC_GRAVE,       KC_CIRC,        
     KC_BSLS,        KC_LABK,        KC_3,           KC_2,           KC_1,           KC_COLN,                                        KC_DQUO,        KC_0,           KC_RCBR,        KC_RPRN,        KC_RBRC,        KC_COMMA,       
     KC_NO,          KC_RABK,        KC_6,           KC_5,           KC_4,           KC_EQUAL,                                       KC_DLR,         KC_QUES,        KC_DOT,         KC_MINUS,       KC_SLASH,       KC_SPACE,       
-                                                    KC_TRANSPARENT, KC_BSPC,                                        KC_BSPC,        LT(2, KC_F23)
+                                                    KC_TRANSPARENT, KC_BSPC,                                        KC_BSPC,        LT(1, KC_F23)
   ),
   [2] = LAYOUT_voyager(
     KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,                             KC_NO,            KC_NO,           LGUI(KC_UP),     KC_NO,           KC_NO,           KC_NO,            
