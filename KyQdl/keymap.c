f607fe6ce8d51cabd682bdb65b87008d325bf4e4
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-09-22 23:18:59 +0100

    refactor: Augment F23 in Mouse

--- KyQdl/keymap.c
+++ KyQdl/keymap.c
@@ -406,7 +406,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     break;
   case MT(MOD_LSFT, KC_F23):
-    if (record->tap.count && record->event.pressed) {
+    if (record->tap.count) {
       repeat_key_invoke(&record->event);
       return false;
     }
