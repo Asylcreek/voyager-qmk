8bc6e06cc59fc2cd3145babe7f9d2944ac01eb41
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-11-21 09:56:44 +0100

    fix: Remove DUAL_FUNC in keymap

--- KyQdl/keymap.c
+++ KyQdl/keymap.c
@@ -76,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [4] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          TOGGLE_SCROLL,  KC_NO,          KC_NO,                                          KC_NO,          LGUI(KC_LEFT),  DUAL_FUNC_0,    LGUI(KC_RIGHT), KC_NO,          KC_NO,          
-    KC_NO,          LCTL(KC_TAB),   KC_MS_BTN2,     DRAG_SCROLL,    KC_MS_BTN1,     LALT(LGUI(LCTL(LSFT(KC_S)))),                                LALT(KC_LEFT),  KC_LEFT,        DUAL_FUNC_1,    KC_RIGHT,       RALT(KC_RIGHT), KC_NO,          
+    KC_NO,          KC_NO,          KC_NO,          TOGGLE_SCROLL,  KC_NO,          KC_NO,                                          KC_NO,          LGUI(KC_LEFT),  UP_CMD_UP,    LGUI(KC_RIGHT), KC_NO,          KC_NO,          
+    KC_NO,          LCTL(KC_TAB),   KC_MS_BTN2,     DRAG_SCROLL,    KC_MS_BTN1,     LALT(LGUI(LCTL(LSFT(KC_S)))),                                LALT(KC_LEFT),  KC_LEFT,        DOWN_CMD_DOWN,    KC_RIGHT,       RALT(KC_RIGHT), KC_NO,          
     KC_NO,          LALT(LGUI(LCTL(LSFT(KC_C)))),LALT(KC_MS_BTN1),LGUI(KC_MS_BTN1),LSFT(KC_MS_BTN1),QK_LLCK,                                        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 MAGIC,         REPEAT
   ),
